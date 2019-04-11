d044631c9bf0fa4ad2ac947729264499920d7f95
Practicas1 <practicas1@Mac-mini-de-Practicas1.local>
2019-04-11 11:18:12 +0200

    ahorasi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EmpresasListComponent} from './empresas-list/empresas-list.component';
 import { EmpresasEditComponent } from './empresas-edit/empresas-edit.component';
-import { OktaCallbackComponent } from '@okta/okta-angular';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
@@ -22,10 +21,6 @@ const routes: Routes = [
   {
     path: 'empresas-edit/:id',
     component: EmpresasEditComponent
-  },
-  {
-    path: 'implicit/callback',
-    component: OktaCallbackComponent
   }
 ];
 
---
+++ src/app/coches-list/coches-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coches-list',
+  templateUrl: './coches-list.component.html',
+  styleUrls: ['./coches-list.component.css']
+})
+export class CochesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
